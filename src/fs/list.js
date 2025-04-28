fd44fa08e1b1a3cae3ffcd1ee344b8db4d1e8130
Natalya Solovyova <ns-66@yandex.ru>
2025-04-28 23:16:50 +0700

    feat: add decisions for File system tasks

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const list = async () => {
-    // Write your code here 
+    const __dirname  = dirname(fileURLToPath(import.meta.url));
+    const dirPath  = join(__dirname, 'files');
+
+    try {
+        const files = await readdir(dirPath);
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname  = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const fileContent = await readFile(filePath, { encoding: 'utf8' });
+        console.log(fileContent);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
