5e7858314f4647a912563a33c22b14d033dcddbc
Natalya Solovyova <ns-66@yandex.ru>
2025-04-29 11:39:47 +0700

    feat: add decisions for Streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('') + '\n';
+            this.push(reversed);
+            callback();
+        }
+    });
+
+    console.log('Enter text (to finish, press Ctrl+C):');
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
+    
+    process.stdin.on('end', () => {
+        stream.end();
+    });
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname  = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+    const stream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+    console.log('Enter text (to finish, press Ctrl+C):');
+
+    process.stdin.on('data', (chunk) => {
+        stream.write(chunk);
+    });
+    
+    process.stdin.on('end', () => {
+        stream.end();
+    });
 };
 
 await write();
