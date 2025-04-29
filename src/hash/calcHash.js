bb43dc68d8f7a63d4a90969c871bfe4a13d38b8e
Natalya Solovyova <ns-66@yandex.ru>
2025-04-29 12:01:43 +0700

    feat: add decisions for hash and zip tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname  = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    
+    const stream = createReadStream(filePath);
+
+    const hash = createHash('sha256');
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+    
+    stream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash)
+    });
 };
 
 await calculateHash();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream';
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname  = dirname(fileURLToPath(import.meta.url));
+    const sourcePath = join(__dirname, 'files', 'fileToCompress.txt');
+    const destinationPath = join(__dirname, 'files', 'archive.gz');
+
+    const gzip = createGzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+
+    pipeline(source, gzip, destination, (error) => {
+        if (error) {
+            console.error('Error', error);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
